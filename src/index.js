115d290c8dc43267269f6bb21b944a2d7d8f58af
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-23 18:54:57 -0300

    import {Link} from 'react-router-dom';

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AutorBox from './Autor';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -10,7 +11,7 @@ ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={App} />
-            <Route path="/autor" />
+            <Route path="/autor" component={AutorBox}/>
             <Route path="/livro" />
         </Switch>
     </BrowserRouter>
