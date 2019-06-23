aef08f8ae01a3b5bbb3c1643f08d8408ef8be707
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-23 18:42:21 -0300

    Configuração do react-router-dom v5

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
-    <App />, 
+    (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/autor" />
+            <Route path="/livro" />
+        </Switch>
+    </BrowserRouter>
+    ),
     document.getElementById('root')
 );
 
