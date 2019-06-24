560ee803773a9357f59a29e07171033d64d837c9
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-06-24 10:16:28 -0300

    Updates

---
+++ precache-manifest.826b6b65306ef0c2c4d3f5b5afb95ca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe56012050fa999d2e4d9c66321886bc",
+    "url": "/project-react/index.html"
+  },
+  {
+    "revision": "d1cfe4b5fde6e5a54eb0",
+    "url": "/project-react/static/css/main.200ce086.chunk.css"
+  },
+  {
+    "revision": "2821aad5ca1c2a9d85ae",
+    "url": "/project-react/static/js/2.66a1a4c5.chunk.js"
+  },
+  {
+    "revision": "d1cfe4b5fde6e5a54eb0",
+    "url": "/project-react/static/js/main.b7ebfe21.chunk.js"
+  },
+  {
+    "revision": "290f747e1d5709d91bf6",
+    "url": "/project-react/static/js/runtime~main.194ee2fc.js"
+  }
+]);
